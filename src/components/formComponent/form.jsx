06e95772a21640c0db06eae389fa3552327c705f
MuraroGuilherme <parkso78@gmail.com>
2024-09-19 10:15:50 -0300

    :ok_hand: style: Changing style features

--- src/components/formComponent/form.jsx
+++ src/components/formComponent/form.jsx
@@ -12,48 +12,48 @@ const Field = React.forwardRef(({label, type}, ref) => {
 
 export const Form = ({onSubmit}) => {
         const emailRef = React.useRef();
-        const confirmEmailRef = React.useRef();
+        const confirmPasswordRef = React.useRef();
         const passwordRef = React.useRef();
-        const [errors, setErrors] = React.useState({ email: "", password: "", confirmEmail: "" });
+        const [errors, setErrors] = React.useState({ email: "", password: "", confirmPassword: "" });
         const [submitted, setSubmitted] = React.useState(false);
 
-        const validate = (email, confirmEmail, password) => {
-          const newErrors = { email: "", password: "", confirmEmail: "" };
+        const validate = (email, password, confirmPassword) => {
+          const newErrors = { email: "", password: "", confirmPassword: "" };
 
-          if (confirmEmail !== email) {
-            newErrors.confirmEmail = "Emails do not match.";
-          }
-      
+          
           const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
           if (!emailRegex.test(email)) {
             newErrors.email = "Insert a valid email.";
           }
-
+          
           if (password.length < 6) {
             newErrors.password = "Password must have at least 6 characters";
           }
-      
+          
+          if (confirmPassword !== password) {
+            newErrors.confirmPassword = "Passwords do not match.";
+          }
           setErrors(newErrors);
 
-          return !newErrors.email && !newErrors.password && !newErrors.confirmEmail;
+          return !newErrors.email && !newErrors.password && !newErrors.confirmPassword;
         }
         const handleSubmit = (event) => {
             event.preventDefault();
         
             const email = emailRef.current.value;
-            const confirmEmail = confirmEmailRef.current.value;
+            const confirmPassword = confirmPasswordRef.current.value;
             const password = passwordRef.current.value;
         
-            if (validate(email, confirmEmail, password)) {
+            if (validate(email, confirmPassword, password)) {
               const data = {
                 email,
-                confirmEmail,
                 password,
+                confirmPassword,
               };
               onSubmit(data);
 
               emailRef.current.value = "";
-              confirmEmailRef.current.value = "";
+              confirmPasswordRef.current.value = "";
               passwordRef.current.value = "";
 
               setSubmitted(true);
@@ -66,10 +66,10 @@ export const Form = ({onSubmit}) => {
                 <div className="inputLabelDiv">
                     <Field ref={emailRef} label="Email" type="Email" />
                     {errors.email && <p className="errorStyle">{errors.email}</p>}
-                    <Field ref={confirmEmailRef} label="Confirm Email" type="Email" />
-                    {errors.confirmEmail && <p className="errorStyle">{errors.confirmEmail}</p>}
                     <Field ref={passwordRef} label="Password" type="Password" />
                     {errors.password && <p className="errorStyle">{errors.password}</p>}
+                    <Field ref={confirmPasswordRef} label="Confirm Password" type="password" />
+                    {errors.confirmPassword && <p className="errorStyle">{errors.confirmPassword}</p>}
                 </div>
                 <div>
                     <button className="submitStyle" type="submit">Submit</button>
